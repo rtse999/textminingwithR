33fb1404b8a2f92f61f671a10ced5a131760d0e6
rtse999 <rtse999@gmail.com>
2018-02-03 14:21:43 +1100

    14:21 - Saturday 3 February 2018

--- TextMiningWithR_Code.R
+++ TextMiningWithR_Code.R
@@ -20,4 +20,40 @@ devtools::session_info()
 # ------------------------------------------------------------------------
 # Libraries
 # ------------------------------------------------------------------------
+library(dplyr)
+library(tidytext)
+library(stringr)
+library(janeaustenr)
+
+# ------------------------------------------------------------------------
+# Chapter 1: The Tidy Text Format
+# ------------------------------------------------------------------------
+text <- c(" Because I could not stop for Death -", 
+          "He kindly stopped for me -", 
+          "The Carriage held but just Ourselves -", 
+          "and Immortality")
+text
+
+text_df <- data_frame(line = 1:4, text = text)
+text_df
+
+text_df %>% unnest_tokens(word, text)
+
+# Jane Austen Example
+original_books <- austen_books() %>% 
+  group_by(book) %>% 
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text,
+                     regex(" ^ chapter [\\ divxlc]", ignore_case = TRUE))
+                     )) %>% 
+  ungroup()
+original_books
+
+tidy_books <- original_books %>% 
+  unnest_tokens(word, text)
+tidy_books
+
+
+
+
 
