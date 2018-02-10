44f8cc5809924faf1744aa0b9efe1e9031b7ed5c
rtse999 <rtse999@gmail.com>
2018-02-10 17:35:00 +1100

    17:34 - Saturday 10 February 2018

--- TextMiningWithR_Code.R
+++ TextMiningWithR_Code.R
@@ -262,3 +262,26 @@ freq_by_rank %>%
   scale_x_log10() +
   scale_y_log10()
 
+book_words <- book_words %>% 
+  bind_tf_idf(word, book, n)
+
+book_words %>% 
+  select(-total) %>% 
+  arrange(desc(tf_idf))
+
+book_words %>% 
+  arrange(desc(tf_idf)) %>% 
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(book) %>% 
+  top_n(15) %>% 
+  ungroup() %>% 
+  ggplot(aes(word, tf_idf, fill = book)) + 
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~book, ncol = 2, scales="free") +
+  coord_flip()
+
+
+  
+          
+          
